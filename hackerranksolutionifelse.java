7cdd807ea1b280b1c7df5e710bd3676a75802162
Anshika Sharma <72012561+anshika-sharma210@users.noreply.github.com>
2021-09-27 10:38:18 -0700

    Add files via upload

---
+++ hackerranksolutionifelse.java
@@ -0,0 +1,25 @@
+import java.io*;
+import java.math.*;
+import java.security.*;
+import java.util.*;
+import java.util.regex.*;
+
+public class solution {
+private static final Scanner scanner = new Scanner(System.in);
+int N = scanner.nextInt();
+scannner.skip("(\r\n |[\r\n\u2028\u2029\u0085])?");
+scanner.close();
+if (N%2==1) {
+System.out.println("weird");
+}
+else if(N%2==0 && (N>=2 && N<=5)) {
+System.out.println("NOT weird");
+}
+else if(N%2==0 && (N>=2 && N<=5)) {
+System.out.println("NOT weird");
+}
+else if(N%2==0 && (N>=20 && N<=100)) {
+System.out.println("NOT weird");
+}
+}
+}
